5e7537ef2c713f2a91ee2e30282c9caca6edbb96
blackshane <122633142+blackshane@users.noreply.github.com>
2023-04-10 12:33:15 -0500

    switch statements and input setup

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,58 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a description of your project'
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Please enter instillation instructions'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message:'Please enter usage information'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message:'Please enter contribution guidelines'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message:'Please enter test instructions'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message:'Please select license from list',
+        choices: [
+            "MIT",
+            "Apache-2.0",
+            "GPL-3.0"
+        ]
+    }
 
+];
+
+inquirer
+    .prompt(questions) ;
+    .then((answers) => {
+
+    });
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile('../README.md', data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,56 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    case "Apache-2.0":
+      return "[![License: Apache-2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    case "GPL-3.0":
+      return "[![License: GPL-3.0](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case "MIT":
+      return "[![License: MIT]](https://opensource.org/licenses/MIT)";
+    case "Apache-2.0":
+      return "[![License: Apache-2.0]](https://opensource.org/licenses/Apache-2.0)";
+    case "GPL-3.0":
+      return "[![License: GPL-3.0]](https://www.gnu.org/licenses/gpl-3.0)";
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  switch (license) {
+    case "MIT":
+      return `
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.`;
+    case "Apache-2.0":
+      return `
+## License
+
+This project is licensed under the Apache License, Version 2.0 - see the [LICENSE](LICENSE) file for details.`;
+    case "GPL-3.0":
+      return `
+## License
+
+This project is licensed under the GNU General Public License v3.0 - see the [LICENSE](LICENSE) file for details.`;
+    default:
+      return "";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
